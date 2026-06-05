f62307c449eb6767d76388fa7f1e0cc9e4ea9fa6
Prince <160013199+princekumar1684@users.noreply.github.com>
2026-06-05 14:20:07 +0530

    updated middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -2,19 +2,17 @@ const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
   try {
-    const authHeader = req.headers.authorization;
+    const token = req.cookies.token;
 
-    if (!authHeader) {
+    if (!token) {
       return res.status(401).json({ message: "No token" });
     }
-
-    const token = authHeader.split(" ")[1];
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     req.user = decoded;
     next();
-  } catch (err) {
+  } catch (error) {
     return res.status(401).json({ message: "Invalid token" });
   }
 };
